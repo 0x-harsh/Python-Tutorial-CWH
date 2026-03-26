82ca0fff26355d5ea520b9848c4d52b0c9625fa0
Harsh Dhiman <harsh61355@gmail.com>
2026-03-26 12:12:18 +0530

    Variables and Datatypes in python

---
+++ 03 Variables and Datatypes/main.py
@@ -0,0 +1,31 @@
+a = 1
+
+b = 2
+
+c = a + b
+
+print(c)
+
+d = "Harry"
+
+e = True
+
+f = None
+
+# INVALID VARIABLE NAMES
+# @sameer = 12
+# 1sameer = 34
+# sam meer = 56
+
+# Data types in python
+# 1. Integer
+# 2. Floating Point Numbers
+# 3. Strings
+# 4. Booleans
+# 5. None
+
+Operators:
+Arithmetic: +, -, *, / etc.
+Assignment: =, -=, +=
+Comparison: <, >, <=, >=, ==
+Logical: and, or, not
