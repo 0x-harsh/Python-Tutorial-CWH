bb62a6cab16941013d338054d69cd5842ee607bd
Harsh Dhiman <harsh61355@gmail.com>
2026-03-26 12:31:57 +0530

    TypeCasting and Input function Practice set

---
+++ 04 Typecasting and Input Function/Practice Set/four.py
@@ -0,0 +1,4 @@
+num = int(input("Enter number: "))
+
+print("The square of the num is", num**2)
+print("The square of the num is", num*num)
---
+++ 04 Typecasting and Input Function/Practice Set/three.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+print("The average is", (num1 + num2)/2)
