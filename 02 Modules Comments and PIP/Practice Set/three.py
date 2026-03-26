4bb0657e2d933699777400bbde9ac090832ba340
Harsh Dhiman <harsh61355@gmail.com>
2026-03-26 11:24:23 +0530

    Modules Comments and PIP practice set

---
+++ 02 Modules Comments and PIP/Practice Set/three.py
@@ -0,0 +1,10 @@
+import os
+
+# directory path
+directory_path = 'C:/Users/ASUS/Desktop/Python Tutorial/02 Modules Comments and PIP/Practice Set/'
+
+# list all files and directories
+contents = os.listdir(directory_path)
+
+# print each file and directory
+print(item)
