5519ad4672fcf6952dedee02c610c0c248487136
Harsh Dhiman <harsh61355@gmail.com>
2026-03-26 11:08:08 +0530

    Modules, Comments and PIP

---
+++ 02 Modules Comments and PIP/main.py
@@ -0,0 +1,10 @@
+import pyjokes # pyjokes is a module
+
+joke = pyjokes.get_joke()
+print(joke)
+
+# Module is a file containing code written by somebody else (usually) which can be imported and used in our programs.
+
+# PIP is the package manager for the python. You can use pip to insall a module on your system.
+
+# pip install flask
