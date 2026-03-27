6cf13cad0ecd6c627c24e52775b74154a7ca4b60
Harsh Dhiman <harsh61355@gmail.com>
2026-03-27 09:16:39 +0530

    Strings Practice Set

---
+++ 05 Strings/Practice Set/three.py
@@ -0,0 +1,4 @@
+string = "I am learning  python"
+
+print(string.find("  "))
+print(string.replace("  ", " "))
---
+++ 05 Strings/Practice Set/two.py
@@ -0,0 +1,5 @@
+letter = '''Dear <|Name|>,
+You are selected!
+<|Date|>'''
+
+print(letter.replace("<|Name|>", "Harsh").replace("<|Date|>", "26 March 2026"))
