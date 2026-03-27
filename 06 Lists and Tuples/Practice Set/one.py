63a7ad136101b5fe5f357658f1c4084be62fefae
Harsh Dhiman <harsh61355@gmail.com>
2026-03-27 10:07:18 +0530

    Lists and Tuples Practice Set

---
+++ 06 Lists and Tuples/Practice Set/one.py
@@ -0,0 +1,18 @@
+fruits = []
+
+fruit = input("Enter fruit name: ")
+fruits.append(fruit)
+fruit = input("Enter fruit name: ")
+fruits.append(fruit)
+fruit = input("Enter fruit name: ")
+fruits.append(fruit)
+fruit = input("Enter fruit name: ")
+fruits.append(fruit)
+fruit = input("Enter fruit name: ")
+fruits.append(fruit)
+fruit = input("Enter fruit name: ")
+fruits.append(fruit)
+fruit = input("Enter fruit name: ")
+fruits.append(fruit)
+
+print(fruits)
---
+++ 06 Lists and Tuples/Practice Set/two.py
@@ -0,0 +1,20 @@
+marks = []
+
+mark = int(input("Enter marks: "))
+marks.append(mark)
+mark = int(input("Enter marks: "))
+marks.append(mark)
+mark = int(input("Enter marks: "))
+marks.append(mark)
+mark = int(input("Enter marks: "))
+marks.append(mark)
+mark = int(input("Enter marks: "))
+marks.append(mark)
+mark = int(input("Enter marks: "))
+marks.append(mark)
+mark = int(input("Enter marks: "))
+marks.append(mark)
+
+marks.sort()
+
+print(marks)
